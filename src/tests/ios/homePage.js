8e3d47d4e62c44d813d705d3a3a02cbbabfff8a3
matiasbrunosp <mbruno@epix.com>
2020-07-15 12:33:33 -0300

    POC iOS Testing working

--- src/tests/ios/homePage.js
+++ src/tests/ios/homePage.js
@@ -1,27 +1,32 @@
-var data = require('../../data/data.json')
-
 module.exports = {
-    // elements: {
-    //     imgLogo: `*[contains(@class, 'android.widget.Image')]`,
-    //     loginBtn: `//android.widget.Button[@text="Log in"]`,
-    //     loadingSpinner: `//*[@text="0.0"]`,
-    //     trendingsContent: `//android.view.View[@text="Timeline: Explore"]`,
-    //     usernameInput: `//android.view.View[@text="Phone, email, or username"]//..//*[contains(@class, 'EditText')]`,
-    //     passwordInput: `//android.view.View[@text="Password"]//..//*[contains(@class, 'EditText')]`,
+    elements: {
+        toogleBtn: `//XCUIElementTypeSwitch[@name="toggleButton"]`
+        // loginBtn: `//android.widget.Button[@text="Log in"]`,
+        // loadingSpinner: `//*[@text="0.0"]`,
+        // trendingsContent: `//android.view.View[@text="Timeline: Explore"]`,
+        // usernameInput: `//android.view.View[@text="Phone, email, or username"]//..//*[contains(@class, 'EditText')]`,
+        // passwordInput: `//android.view.View[@text="Password"]//..//*[contains(@class, 'EditText')]`,
 
-    //     directMessagesBtn: `//android.view.View[@text="Direct Messages"]`,
-    //     startConversationBtn: `//android.widget.Button[contains(@text, 'Start a conversation')]`,
-    //     peopleSearchbox: `//*[contains(@class, 'EditText')]`
-    // },
+        // directMessagesBtn: `//android.view.View[@text="Direct Messages"]`,
+        // startConversationBtn: `//android.widget.Button[contains(@text, 'Start a conversation')]`,
+        // peopleSearchbox: `//*[contains(@class, 'EditText')]`
+    },
 
-    'POC Android Testing': function (nightwatch) {
+    'POC iOS Testing': function (nightwatch) {
         console.log("ios test");
-        // const landingPage = nightwatch.page.androidLandingPage();
-        // landingPage.isTrendingsContentVisible();
-        // const loginPage = landingPage.goToLoginPage(nightwatch);
-        // loginPage.setUsername(data.user);
-        // loginPage.setPassword(data.password);
-        // loginPage.clickLogin(nightwatch);
-        // nightwatch.pause(5000);
+        nightwatch.pause(10000);
+        nightwatch.useXpath();
+        nightwatch.waitForElementPresent(this.elements.toogleBtn, 10000)
+        nightwatch.click(this.elements.toogleBtn);
+        nightwatch.pause(3000);
+        nightwatch.click(this.elements.toogleBtn);
+        nightwatch.pause(3000);
+        nightwatch.click(this.elements.toogleBtn);
+        nightwatch.pause(3000);
+        nightwatch.click(this.elements.toogleBtn);
+        nightwatch.pause(3000);
+        nightwatch.click(this.elements.toogleBtn);
+        nightwatch.pause(3000);
+        nightwatch.click(this.elements.toogleBtn);
     }
 }
