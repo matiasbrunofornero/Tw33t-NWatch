559ceabe5de25bd9b16df822ca032e1ca6648226
matiasbrunosp <mbruno@epix.com>
2020-07-21 19:03:34 -0300

    Full API Tests Refactor

--- src/api/twitterSpec.js
+++ src/api/twitterSpec.js
@@ -1,74 +1,149 @@
 var SyncRequest = require('sync-request');
-var userToOperateWith = null;
 var BaseURLApi = 'https://postman-twitter.herokuapp.com/';
 
 describe("Twitter-like API for Postman internship", function () {
 
     it("Basic Twitter Heroku flow", function () {
 
-        userToOperateWith = {
+        sUser = {
             "name": "Sayantan",
             "username": "sayantan",
             "password": "secretpwd"
         };
 
-        var oRequestCreate = SyncRequest("POST", BaseURLApi + 'api/auth/register', {
-            json: userToOperateWith
+        let oRequestCreate = SyncRequest("POST", BaseURLApi + 'api/auth/register', {
+            json: sUser
         });
 
-        var oResponseCreate = JSON.parse(oRequestCreate.body);
-        const userId = oResponseCreate.user_id;
+        let oResponseCreate = JSON.parse(oRequestCreate.body);
+        let sAuth = oResponseCreate.auth;
+        let sToken = oResponseCreate.token;
+        const sUserId = oResponseCreate.user_id;
 
-        userToOperateWith = {
-            "username": "sayantan",
-            "password": "secretpwd"
-        };
+        expect(sAuth).toBe(true);
+        expect(sToken).toMatch(new RegExp("\.{171}"));
 
-        var oRequestCreate = SyncRequest("POST", BaseURLApi + 'api/auth/login', {
-            json: userToOperateWith
+        oRequestCreate = SyncRequest("POST", BaseURLApi + 'api/auth/login', {
+            json: {
+                "username": sUser.username,
+                "password": sUser.password
+            }
         });
 
-        var oResponseCreate = JSON.parse(oRequestCreate.body);
-        var token = oResponseCreate.token
+        oResponseCreate = JSON.parse(oRequestCreate.body);
+        sToken = oResponseCreate.token;
 
-        var oRequestCreate = SyncRequest("GET", BaseURLApi + 'api/auth/me', {
+        expect(sAuth).toBe(true);
+        expect(sToken).toMatch(new RegExp("\.{171}"));
+
+        oRequestCreate = SyncRequest("GET", BaseURLApi + 'api/auth/me', {
             headers: {
-                "Authorization": "Bearer " + token,
+                "Authorization": "Bearer " + sToken,
                 "Content-Type": "application/x-www-form-urlencoded",
                 "Host": "postman-twitter.herokuapp.com",
                 "X-Amz-Date": "20181031T171842Z"
             },
         })
 
-        var oResponseCreate = JSON.parse(oRequestCreate.body);
+        oResponseCreate = JSON.parse(oRequestCreate.body);
+        const sName = oResponseCreate.name;
+        const sUsername = oResponseCreate.username;
 
-        var oRequestCreate = SyncRequest("GET", BaseURLApi + 'api/auth/logout', {
+        expect(sName).toEqual(sUser.name);
+        expect(sUsername).toEqual(sUser.username);
+
+        oRequestCreate = SyncRequest("GET", BaseURLApi + 'api/auth/logout', {
             headers: {
-                "Authorization": "Bearer " + token
-            },
+                "Authorization": "Bearer " + sToken
+            }
         })
 
-        var oResponseCreate = JSON.parse(oRequestCreate.body);
+        oResponseCreate = JSON.parse(oRequestCreate.body);
+        sAuth = oResponseCreate.auth;
 
-        userToOperateWith = {
+        expect(sAuth).toBe(false);
+        expect(oResponseCreate.token).toBeNull();
+
+        aUser = {
             "name": "Argha",
             "username": "argha",
             "password": "secretpwd"
         };
 
-        var oRequestCreate = SyncRequest("POST", BaseURLApi + 'api/auth/register', {
-            json: userToOperateWith
+        oRequestCreate = SyncRequest("POST", BaseURLApi + 'api/auth/register', {
+            json: aUser
         });
 
-        var oResponseCreate = JSON.parse(oRequestCreate.body);
-        // bToken = oResponseCreate.token
+        oResponseCreate = JSON.parse(oRequestCreate.body);
+        const aAuth = oResponseCreate.auth;
+        const aToken = oResponseCreate.token;
+        const aUserId = oResponseCreate.user_id;
 
-        var oRequestCreate = SyncRequest("POST", BaseURLApi + 'api/user/' + userId + '/follow', {
-            "Authorization": "Bearer " + oResponseCreate.token,
+        expect(aAuth).toBe(true);
+        expect(aToken).toMatch(new RegExp("\.{171}"));
+
+        oRequestCreate = SyncRequest("POST", BaseURLApi + 'api/user/' + sUserId + '/follow', {
+            headers: {
+                "Authorization": "Bearer " + aToken
+            }
         });
 
-        var oResponseCreate = JSON.parse(oRequestCreate.body);
-        console.log(oResponseCreate)
+        oResponseCreate = JSON.parse(oRequestCreate.body);
 
-    });
-});
+        expect(oResponseCreate.from).toEqual(aUserId);
+        expect(oResponseCreate.to).toEqual(sUserId);
+
+        oRequestCreate = SyncRequest("POST", BaseURLApi + 'api/user/' + sUserId + '/unfollow', {
+            headers: {
+                "Authorization": "Bearer " + aToken
+            }
+        });
+
+        const tweet = "Hello from the server side!";
+        oRequestCreate = SyncRequest("POST", BaseURLApi + 'api/tweet/new', {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': "Bearer " + sToken
+            },
+            body: JSON.stringify({ "text": tweet }),
+        });
+
+        oResponseCreate = JSON.parse(oRequestCreate.body);
+        const tweetId = oResponseCreate._id;
+        expect(tweetId).toMatch(new RegExp("\.{24}"));
+
+        oRequestCreate = SyncRequest("GET", BaseURLApi + 'api/tweet/' + tweetId, {
+            headers: {
+                "Authorization": "Bearer " + sToken
+            }
+        });
+
+        // STATUS CODE EQUALS TO 200 (SUCCESSFULL!!)
+        // REPEAT IN ALL THE REQUESTS
+        expect(oRequestCreate.statusCode).toEqual(200);
+
+        oResponseCreate = JSON.parse(oRequestCreate.body);
+        expect(oResponseCreate.nLikes).toEqual(0);
+        expect(oResponseCreate.isReply).toBe(false);
+        expect(oResponseCreate.text).toEqual(tweet);
+
+        oRequestCreate = SyncRequest("POST", BaseURLApi + 'api/tweet/' + tweetId + '/like', {
+            headers: {
+                "Authorization": "Bearer " + sToken
+            }
+        });
+
+        console.log(JSON.parse(oRequestCreate.body))
+
+        oRequestCreate = SyncRequest("DELETE", BaseURLApi + 'api/tweet/' + tweetId, {
+            headers: {
+                "Authorization": "Bearer " + sToken
+            }
+        });
+
+        expect(oRequestCreate.statusCode).toEqual(200);
+        oResponseCreate = JSON.parse(oRequestCreate.body);
+        expect(oResponseCreate.message).toEqual('Successfully deleted!');
+
+    })
+})
